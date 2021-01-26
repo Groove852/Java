a6a2678e72a6ab9107280d3b10251a6fc8ff3fef
Groove852 <gaganertime@mail.com>
2021-01-26 18:12:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer a = 12;
+        Integer b = 10;
+        System.out.println(a > b ? 12 : 10); // (выражение)?(если истинно):(если ложно)
+    }
+}
