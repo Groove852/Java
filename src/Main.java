bf4c9643ae68e744030ce11d47a6c7ff1b07b39f
Groove852 <gaganertime@mail.com>
2021-01-26 19:36:12 +0300

    Homework 26.01.2021(1)

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Reading.consoleHelper ConsoleHelper = consoleHelper.getInstance();
         Mathematic.myMath myMathematic = myMath.getInstance();
-        System.out.println(myMathematic.exponentiation(Double.parseDouble(ConsoleHelper.readMessage()), Double.parseDouble(ConsoleHelper.readMessage())));
+        System.out.println(myMath.exponentiation(Double.parseDouble(ConsoleHelper.readMessage()), Double.parseDouble(ConsoleHelper.readMessage())));
 
     }
 }
